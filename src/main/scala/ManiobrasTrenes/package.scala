c2309be52c990021a1b6b6ef4dba69da5615ce4e
elkin <elkin.21.74@gmail.com>
2025-04-02 14:30:07 -0500

    segundo commit

--- src/main/scala/ManiobrasTrenes/package.scala
+++ src/main/scala/ManiobrasTrenes/package.scala
@@ -1,3 +1,52 @@
 package object ManiobrasTrenes {
+  type Vagon = Any
+  type Tren = List[Vagon]
+  type Estado = (Tren, Tren, Tren)
+
+  trait Movimiento
+
+  case class Uno(n:Int) extends Movimiento
+  case class Dos(n:Int) extends Movimiento
+
+  type Maniobra = List[Movimiento]
+
+  def aplicarMovimiento(e:Estado, m:Movimiento): Estado = {
+    val (principal, uno, dos) = e
+     m match {
+       case Uno(0) => e
+       case Dos(0) => e
+       case Uno(n) =>
+         if (n > 0)
+           val cantidadAMover = math.min(n, principal.length)
+           val (quedaEnPrincipal, paraUno) = principal.splitAt(principal.length - cantidadAMover)
+           (quedaEnPrincipal, paraUno ++ uno, dos)
+         else
+           val cantidadAMover = math.min(-n, uno.length)
+           val (paraPrincipal, quedaEnUno) = uno.splitAt(cantidadAMover)
+           (principal ++ paraPrincipal, quedaEnUno, dos)
+
+       case Dos(n) => {
+         if (n>0)
+           val cantidadAMover = math.min(n, principal.length)
+           val (quedaEnPrincipal, paraDos) = principal.splitAt(principal.length - cantidadAMover)
+           (quedaEnPrincipal, uno, paraDos ++ dos)
+         else
+           val cantidadAMover = math.min(-n, dos.length)
+           val (paraPrincipal, quedaEnDos) = dos.splitAt(cantidadAMover)
+           (principal ++ paraPrincipal, uno, quedaEnDos)
+       }
+
+     }
+  }
+
+  def aplicarMovimientos(e:Estado, movs:Maniobra): List[Estado] = {
+    movs match
+      case Nil => List(e)
+      case x::xs => e :: aplicarMovimientos(aplicarMovimiento(e,x),xs)
+  }
+
+  def definirManiobra(t1:Tren, t2:Tren):Maniobra = {
+    List()
+  }
 
 }
---
+++ src/test/scala/Pruebas.sc
@@ -0,0 +1,15 @@
+import ManiobrasTrenes._
+
+val e1 = (List('a', 'b', 'c', 'd'), Nil, Nil)
+val e2 = aplicarMovimiento(e1, Uno(2))
+val e3 = aplicarMovimiento(e2, Dos(3))
+val e4 = aplicarMovimiento(e3, Dos(-1))
+val e5 = aplicarMovimiento(e4, Uno(-2))
+
+val e6 = aplicarMovimiento(e1,Uno(0))
+val e7 = aplicarMovimiento(e1,Uno(-2))
+
+aplicarMovimientos(e1, List(Uno(2), Dos(3), Dos(-1), Uno(-2), Dos(-1)))
+aplicarMovimientos(e1,List())
+val e = (List('a', 'b'), List('c'), List('d'))
+aplicarMovimientos(e, List(Uno(1), Dos(1), Uno(-2)))
