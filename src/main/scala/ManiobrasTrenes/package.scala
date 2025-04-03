98a45bb3bd968cf9177fe03065f9ac0bdee36bff
elkin <elkin.21.74@gmail.com>
2025-04-02 21:20:15 -0500

    segundo commit

--- src/main/scala/ManiobrasTrenes/package.scala
+++ src/main/scala/ManiobrasTrenes/package.scala
@@ -25,7 +25,7 @@ package object ManiobrasTrenes {
            val (paraPrincipal, quedaEnUno) = uno.splitAt(cantidadAMover)
            (principal ++ paraPrincipal, quedaEnUno, dos)
 
-       case Dos(n) => 
+       case Dos(n) =>
          if (n>0)
            val cantidadAMover = math.min(n, principal.length)
            val (quedaEnPrincipal, paraDos) = principal.splitAt(principal.length - cantidadAMover)
@@ -34,7 +34,7 @@ package object ManiobrasTrenes {
            val cantidadAMover = math.min(-n, dos.length)
            val (paraPrincipal, quedaEnDos) = dos.splitAt(cantidadAMover)
            (principal ++ paraPrincipal, uno, quedaEnDos)
-       
+
 
      }
   }
@@ -46,7 +46,25 @@ package object ManiobrasTrenes {
   }
 
   def definirManiobra(t1:Tren, t2:Tren):Maniobra = {
-    List()
+
+    def machMovimientos(e:Estado, t2:Tren, movs:Maniobra, i:Int): Maniobra= {
+      t2 match
+        case x::Nil => movs.reverse
+        case x::xs =>
+          val mov = Uno(e._1.length - e._1.indexOf(x))
+          val e1 = aplicarMovimiento(e,mov)
+          val mov2 = Dos(e1._1.length - i)
+          val e2 = aplicarMovimiento(e1,mov2)
+          val mov3 = Uno(-e2._2.length)
+          val e3 = aplicarMovimiento(e2,mov3)
+          val mov4 = Dos(-e3._3.length)
+          val e4 = aplicarMovimiento(e3,mov4)
+          machMovimientos(e4,xs,mov4::mov3::mov2::mov::movs,i+1)
+    }
+    machMovimientos((t1,Nil,Nil),t2,Nil,0)
   }
+
+
+
 
 }
--- src/test/scala/Pruebas.sc
+++ src/test/scala/Pruebas.sc
@@ -1,5 +1,5 @@
 import ManiobrasTrenes._
-
+// Pruebas del documento
 val e1 = (List('a', 'b', 'c', 'd'), Nil, Nil)
 val e2 = aplicarMovimiento(e1, Uno(2))
 val e3 = aplicarMovimiento(e2, Dos(3))
@@ -10,6 +10,12 @@ val e6 = aplicarMovimiento(e1,Uno(0))
 val e7 = aplicarMovimiento(e1,Uno(-2))
 
 aplicarMovimientos(e1, List(Uno(2), Dos(3), Dos(-1), Uno(-2), Dos(-1)))
-aplicarMovimientos(e1,List())
 val e = (List('a', 'b'), List('c'), List('d'))
 aplicarMovimientos(e, List(Uno(1), Dos(1), Uno(-2)))
+definirManiobra(List('a','b','c','d'),List('d','b','c','a'))
+
+
+//Pruebas iniciales
+
+val maniobra = definirManiobra(List('a', 'b', 'c', 'd'),List('b', 'a', 'd' ,'c'))
+aplicarMovimientos((List('a', 'b', 'c', 'd'),Nil,Nil) ,maniobra)
